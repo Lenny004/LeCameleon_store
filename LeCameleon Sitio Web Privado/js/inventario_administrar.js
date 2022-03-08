2c6275e7720505036b04f27f075519590030139b
Lenny004 <lennyx004@gmail.com>
2022-03-08 05:35:50 -0600

    Inventario terminado

---
+++ LeCameleon Sitio Web Privado/js/inventario_administrar.js
@@ -0,0 +1,22 @@
+//Evento que se ejecuta cuando se carga la página web
+document.addEventListener('DOMContentLoaded', function() {
+    //Var se crea para variables globales
+    //let es variables locales
+
+    //Instanciar el menú
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+
+    //Instanciar Dropdown Menú
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, {coverTrigger:false, hover: true});
+
+    //Instanciar ToolTips footer
+    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+
+    //Instanciar Modal
+    modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+
+    //Instanciar Select 'Combobox'
+    M.FormSelect.init(document.querySelectorAll('select'));
+});
