2df4db1a9498ca9af4db36dac86fa98e3076802a
Lenny004 <lennyx004@gmail.com>
2022-06-28 19:33:34 -0600

    Cambios en registro

--- api/sitio_privado/api_registro_usuario_empleado.php
+++ api/sitio_privado/api_registro_usuario_empleado.php
@@ -12,7 +12,6 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('estado' => 0, 'session' => 0, 'message' => null, 'exception' => null, 'dataset' => null, 'username' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['idusuario_e'])) {    
         // Se compara la acción a realizar cuando el administrador no ha iniciado sesión.
         switch ($_GET['action']){
             //Verificamos si existen usuarios
@@ -74,9 +73,6 @@ if (isset($_GET['action'])) {
         header('content-type: application/json; charset=utf-8');
         // Se imprime el resultado en formato JSON y se retorna al controlador.
         print(json_encode($result));
-    } else {
-        print(json_encode('Acceso denegado'));
-    }    
 } else {
     print(json_encode('Recurso no disponible'));
 }
