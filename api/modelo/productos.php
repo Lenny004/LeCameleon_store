90f815e86a1ec88538d36973caa5b0ee916cbd4d
Lenny004 <lennyx004@gmail.com>
2022-07-22 04:49:25 -0600

    grafico ventas personalizado

--- api/modelo/productos.php
+++ api/modelo/productos.php
@@ -31,6 +31,9 @@ class Productos extends Validator
     private $total_resenia = null;
     private $min = 0;
     private $max = null;
+    private $filtro = null;
+    private $opcion = null;
+    private $parametro = "";
 
     /*
     *   Métodos para validar y asignar valores de los atributos.
@@ -266,6 +269,26 @@ class Productos extends Validator
         }
     }
 
+    public function setFiltro($value)
+    {
+        if ($this->validacionNumeroNaturales($value)) {
+            $this->filtro = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setOpcion($value)
+    {
+        if ($this->validacionNumeroNaturales($value)) {
+            $this->opcion = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /*
     *   Métodos para obtener valores de los atributos.
     */
@@ -292,6 +315,11 @@ class Productos extends Validator
     public function getTotalResenia()
     {
         return $this->total_resenia;
+    }
+
+    public function getFiltro()
+    {
+        return $this->filtro;
     }
 
     //Petición para traer los colores
@@ -657,5 +685,38 @@ class Productos extends Validator
                 ORDER BY idproducto ASC LIMIT 6 OFFSET 0';
         $params = null;
         return Database::obtenerSentencias($sql, $params);
+    }
+
+    public function filtroProductosVendidos()
+    {   
+        if ($this->filtro == 1) {
+            $sql = 'SELECT tp.idproducto, tp.nombre_producto, SUM(tdf.cantidad_producto) as cantidad_vendida
+            FROM tbproducto tp 
+            INNER JOIN tbdetalle_factura tdf USING(idproducto)
+            INNER JOIN tbfactura tf ON tf.idfactura = tdf.idfactura
+            INNER JOIN tbsubcategoria_producto tsc ON tp.idsubcategoria_producto = tsc.idsubcategoria_producto
+            INNER JOIN tbcategoria tc ON tsc.idsubcategoria_producto = tc.idcategoria_producto
+            WHERE tf.idestado_factura = 1 OR tc.idcategoria_producto = ?
+            GROUP BY tp.idproducto';
+            $params = array($this->opcion);
+            return Database::obtenerSentencias($sql, $params);
+        } else {
+            switch ($this->getFiltro()){
+                case 2:
+                    $this->parametro = "idsubcategoria_producto";
+                case 3:
+                    $this->parametro = "id_marca";
+                case 4:
+                    $this->parametro = "iddistribuidor";
+            }
+            $sql = 'SELECT tp.idproducto, tp.nombre_producto, SUM(tdf.cantidad_producto) as cantidad_vendida
+            FROM tbproducto tp 
+            INNER JOIN tbdetalle_factura tdf USING(idproducto)
+            INNER JOIN tbfactura tf ON tf.idfactura = tdf.idfactura
+            WHERE tf.idestado_factura = 1 AND ? = ?
+            GROUP BY tp.idproducto';
+            $params = array(strval($this->parametro), $this->opcion);
+            return Database::obtenerSentencias($sql, $params);
+        }
     }
 }
--- controladores/privado/graficas_ventas.js
+++ controladores/privado/graficas_ventas.js
@@ -2,6 +2,7 @@ const ENDPOINT_CATEGORIA = SERVER + "sitio_privado/api_categorias.php?action=rea
 const ENDPOINT_SUBC = SERVER + 'sitio_privado/api_subcategoria.php?action=readAll';
 const ENDPOINT_MARCA = SERVER + 'sitio_privado/api_marca.php?action=readAll';
 const ENDPOINT_PROVEEDOR = SERVER + 'sitio_privado/api_proveedor.php?action=readAll';
+const API_PRODUCTOS = SERVER + 'sitio_privado/api_producto.php?action=';
 
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', function () {
@@ -30,28 +31,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//Variable para saber que filtro eligio
+var tipo;
+var tipo_frase;
+//Variable para saber que opción ha elegido
+var opcion;
+
+//Funcion para seleccionar un filtro para cargar en el select de opciones
 function seleccionFiltro(opcion) {
     switch (opcion) {
         case "1":
             //Traemos todas las categorias existentes
             fillSelect(ENDPOINT_CATEGORIA, 'una categoría', 'opciones', null);
+            tipo = 1;
+            tipo_frase = "una categoria";
             break;
         case "2":
             //Traemos todas las subcategorias existentes
             fillSelect(ENDPOINT_SUBC, 'una subcategoría', 'opciones', null);
+            tipo = 2;
+            tipo_frase = "una subcategoría";
             break;
         case "3":
             //Traemos todas las marcas existentes
             fillSelect(ENDPOINT_MARCA, 'una marca', 'opciones', null);
+            tipo = 3;
+            tipo_frase = "una marca";
             break;
         case "4":
             //Traemos todos los proveedores existentes
             fillSelect(ENDPOINT_PROVEEDOR, 'un proveedor', 'opciones', null);
+            tipo = 4;
+            tipo_frase = "un proveedor";
             break;
         default:
             console.log("no entre a ningún caso");
             break;
     }
+}
+
+function seleccionOpcion(opcion_elegida){
+    opcion = opcion_elegida;
 }
 
 // Método manejador de eventos que se ejecuta cuando se envía el formulario de generar gráfica
@@ -60,30 +80,31 @@ document.getElementById('form_personalizado').addEventListener('submit', functio
     event.preventDefault();
     // Se define un objeto con los datos del registro seleccionado.
     const data = new FormData();
-    data.append('ide', id);
+    data.append('tipo', tipo);
+    data.append('opcion', opcion);
     // Petición para obtener los datos del gráfico.
-    fetch(API_PRODUCTOS + 'cantidadProductosCategoria', {
+    fetch(API_PRODUCTOS + 'productosVendidosFiltro', {
         method: 'post',
-        body: 
+        body: data
     }).then(function (request) {
             // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
             if (request.ok) {
                 request.json().then(function (response) {
                     // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
-                    if (response.status) {
+                    if (response.estado) {
                         // Se declaran los arreglos para guardar los datos a graficar.
-                        let categorias = [];
                         let cantidades = [];
+                        let nombre_producto = [];
                         // Se recorre el conjunto de registros devuelto por la API (dataset) fila por fila a través del objeto row.
                         response.dataset.map(function (row) {
                             // Se agregan los datos a los arreglos.
-                            categorias.push(row.nombre_categoria);
-                            cantidades.push(row.cantidad);
+                            cantidades.push(row.cantidad_vendida);
+                            nombre_producto.push(row.nombre_producto);
                         });
                         // Se llama a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
-                        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+                        barGraph('grafica5', cantidades, nombre_producto, 'Cantidad de productos', ('Nombres de productos por '+ tipo_frase));
                     } else {
-                        document.getElementById('chart1').remove();
+                        document.getElementById('grafica5').remove();
                         console.log(response.exception);
                     }
                 });
