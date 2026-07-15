90268559353e2a3ada0e4c04d5d4947f2b05b1e5
Lenny004 <lennyx004@gmail.com>
2026-07-15 06:02:34 -0600

    :seo: include static trust pages in sitemap controller

--- app/Http/Controllers/Store/SitemapController.php
+++ app/Http/Controllers/Store/SitemapController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Services\CatalogService;
 use Illuminate\Http\Response;
 
+/**
+ * XML sitemap for published products and key public pages.
+ */
 class SitemapController extends Controller
 {
     public function __construct(
@@ -16,8 +19,25 @@ class SitemapController extends Controller
     {
         $products = $this->catalogService->publishedForSitemap();
 
+        // Static marketing and trust pages that should be crawlable.
+        $staticPaths = [
+            route('home'),
+            route('shop.index'),
+            route('about'),
+            route('faq'),
+            route('care'),
+            route('shipping'),
+            route('returns'),
+            route('privacy'),
+            route('terms'),
+            route('contact.show'),
+        ];
+
         return response()
-            ->view('sitemap', compact('products'))
+            ->view('sitemap', [
+                'products' => $products,
+                'staticUrls' => $staticPaths,
+            ])
             ->header('Content-Type', 'application/xml');
     }
 }
