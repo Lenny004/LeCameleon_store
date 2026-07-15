69e17c74d124bcc13670ba1898b75d3b3bb406bf
Lenny004 <lennyx004@gmail.com>
2026-07-15 06:02:43 -0600

    :seo: render static URLs in sitemap.xml template

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+@foreach ($staticUrls as $staticUrl)
+    <url>
+        <loc>{{ $staticUrl }}</loc>
+    </url>
+@endforeach
 @foreach ($products as $product)
     <url>
         <loc>{{ route('shop.show', $product->slug) }}</loc>
